62a7dff77012a8ad91ff63d119a6162a82e065ae
JAYBEkster <none>
2016-02-07 14:52:55 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# package-checker
+A simple plugin to compare currently installed packages with list in package.json file.
---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var semver = require('semver'),
+    prcoess = require('process'),
+    Spinner = require('cli-spinner').Spinner,
+    readPackageJson = require('./lib/read-package-json'),
+    getCurrentNpmLs = require('./lib/get-current-npm-ls');
+
+const SETTINGS = {
+    path: process.cwd() + '/package.json',
+    directory: process.cwd(),
+    prod: true,
+    dev: true
+};
+
+/**
+ * checkDeps check fiddeferecies between version from npm ls command and package.json
+ * @param  {Object} options
+ * @return {undefined}
+ */
+function checkDeps(options) {
+    var spinner = new Spinner('%s'),
+        options = Object.assign(SETTINGS, options);
+
+    spinner.setSpinnerString('|/-\\');
+    spinner.start();
+
+    return Promise.all([
+        readPackageJson(options),
+        getCurrentNpmLs(options)
+    ]).then(function(values) {
+        var packageName,
+            output = {},
+            currentPackages = values[1],
+            actualPackages = values[0];
+
+        spinner.stop();
+        process.stdout.write('\n');
+
+        for (packageName in actualPackages) {
+            output[packageName] = {};
+            output[packageName].actualVersion = actualPackages[packageName];
+            if (!currentPackages[packageName] || currentPackages[packageName].missing) {
+                output[packageName].error = 'is missing';
+                continue;
+            }
+
+            if (actualPackages[packageName]
+                && currentPackages[packageName].version !== actualPackages[packageName]
+                && !semver.satisfies(currentPackages[packageName].version, actualPackages[packageName])
+            ) {
+                output[packageName].error = 'invalid version';
+                output[packageName].actualVersion = actualPackages[packageName];
+                output[packageName].relevantVersion = currentPackages[packageName].version;
+            }
+        }
+        return output;
+    }, function(err) {
+        console.log(err);
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+/**
+ * runCheckDeps
+ * @param  {Object}   config
+ * @param  {Function} callback
+ * @return {Promise}
+ */
+function runCheckDeps(config, callback) {
+    var error = null,
+        callback = typeof config === 'object' ? callback : config;
+
+    return checkDeps(config || {}).then(function(output) {
+        callback(error, output)
+    }).catch(function(error) {
+        callback(error, null);
+    });
+}
+
+module.exports = runCheckDeps;
