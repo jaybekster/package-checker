f70af2b0670829da0e8d788c4af6738f5be821cd
JAYBEkster <none>
2016-02-17 21:55:15 +0300

    fix promise

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 
 var semver = require('semver'),
     prcoess = require('process'),
+    textTable = require('text-table'),
+    chalk = require('chalk'),
     version = require('./package.json').version,
     readPackageJson = require('./lib/read-package-json'),
     getCurrentNpmLs = require('./lib/get-current-npm-ls');
+
+const textTableObj = {
+    header: [
+        ['Name', 'Current', 'Wanted', 'Problem']
+    ],
+    options: {
+        align: ['l', 'l', 'l', 'l']
+    }
+};
 
 const SETTINGS = {
     path: process.cwd() + '/package.json',
@@ -34,6 +45,7 @@ function packageChecker(options, callback) {
             currentPackages = values[1],
             actualPackages = values[0],
             packageVersion,
+            tableBody = [],
             packageJsonVersion;
 
         for (packageName in actualPackages) {
@@ -41,7 +53,7 @@ function packageChecker(options, callback) {
             output[packageName].actualVersion = actualPackages[packageName];
 
             if (!currentPackages[packageName] || currentPackages[packageName].missing) {
-                output[packageName].error = 'is missing';
+                tableBody.push([packageName, '', actualPackages[packageName], 'is missing'])
                 continue;
             }
 
@@ -56,14 +68,22 @@ function packageChecker(options, callback) {
                 && packageVersion !== packageJsonVersion
                 && !semver.satisfies(packageVersion, packageJsonVersion)
             ) {
-                output[packageName].error = 'invalid version';
                 output[packageName].actualVersion = packageJsonVersion;
                 output[packageName].relevantVersion = packageVersion;
+                tableBody.push([packageName, packageVersion, packageJsonVersion, 'invalid version']);
             }
         }
-
+        if (tableBody.length) {
+            console.log(
+                textTable([].concat(textTableObj.header, tableBody), textTableObj.options)
+            );
+            throw new Error('Differences were found');
+        } else {
+            console.log(chalk.green('No differences were found'));
+        }
         callback(null, output);
-    }, function(error) {
+    }).catch(function(error) {
+        console.log(chalk.red(error));
         callback(error, null);
     });
 }
